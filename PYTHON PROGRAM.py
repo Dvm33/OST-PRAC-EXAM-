e60e246f98de933cdf51646d8cc402a1058c1565
Dvm33 <96166334+Dvm33@users.noreply.github.com>
2022-01-20 12:41:02 +0530

    Create PYTHON PROGRAM.py

---
+++ PYTHON PROGRAM.py
@@ -0,0 +1,18 @@
+#Python program tgat acts as a mini calculator 
+a=int(input("Enter 1st Number"))
+b=int(input("Enter 2nd Number"))
+operator=input("Enter the operation to be performed")
+if operator=="+":
+  print("Addition is ",a+b)
+elif operator=="*":
+  print("Multiplication is ",a*b)
+elif operator=="/":
+  print("Division is ",a/b)
+elif operator=="//":
+    print("Float Division is:",a//b)
+elif operator=="%"
+    print ("Modulas is :",a%b)
+else :
+  print("Invalid Operator")
+    
+  
